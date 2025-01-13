0c185688707a8dbc01b8b89013586d7d46367def
DavidIncarbone <david.incarbone@gmail.com>
2025-01-13 14:35:41 +0100

    Create custom provider for postlist API's call

---
+++ src/context/GlobalContext.jsx
@@ -0,0 +1,53 @@
+// IMPORT
+
+import { createContext, useContext, useEffect, useState } from "react";
+import axios from "axios";
+
+// VARIABLES
+
+const GlobalContext = createContext();
+const apiUrl = "http://localhost:3000/";
+
+const GlobalProvider = ({ children }) => {
+
+    // VARIABLES
+    const [postsList, setPostsList] = useState([]);
+
+    // FUNCTIONS
+
+    useEffect(() => {
+
+        getPosts();
+
+    }, []);
+
+
+    function getPosts() {
+        axios
+            .get(apiUrl + "/posts")
+            .then((res) => {
+
+                setPostsList(res.data.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+            .finally(() => {
+                console.log("Always executed")
+            });
+    }
+
+    return (
+        <GlobalContext.Provider value={{ postsList }}>
+            {children}
+        </GlobalContext.Provider>
+    );
+
+}
+
+function useGlobalContext() {
+    const context = useContext(GlobalContext);
+    return context;
+}
+
+export { GlobalProvider, useGlobalContext };
